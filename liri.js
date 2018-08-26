194da791cc8f079dc85a3a2399ff63161a4a9dd4
Grace Yoo <graceyoo33@gmail.com>
2018-08-26 12:56:58 -0400

    first and second commands functional

--- liri.js
+++ liri.js
@@ -1,18 +1,56 @@
 require("dotenv").config();
 
+var keys = require('./keys.js');
+var Twitter = require('twitter');
+var Spotify = require('node-spotify-api');
 var request = require("request");
 var fs = require("fs");
 
 var fourCommands = process.argv[2];
 var searchItem = process.argv.slice(3).join(" ");
+var client = new Twitter(keys.twitter);
+var spotify = new Spotify(keys.spotify);
 
 if (fourCommands === "my-tweets") {
-    console.log("A");
+    // console.log("A");
+
+    var params = {
+        screen_name: "testcsbhw",
+        count: 20
+    };
+
+    client.get('statuses/user_timeline', params, function (error, tweets, response) {
+        if (!error) {
+            for (var i = 0; i < tweets.length; i++) {
+                var showTweets = [
+                    "Tweet: " + tweets[i].text,
+                    "Tweeted at: " + tweets[i].created_at
+                ].join("\n\n");
+                console.log("\n" + showTweets + "\n");
+            }
+        }
+    });
 }
 else if (fourCommands === "spotify-this-song") {
-    console.log("B");
+    // console.log("B");
+
+    // Run a request ot the Spotify API with song selected
+    spotify.search({ type: 'track', query: searchItem, limit: 2 }).then(function (response) {
+        var song = response.tracks.items[0];
+        var showSong = [
+            "Artist(s): " + song.artists[0].name,
+            "Song Title: " + song.name,
+            "Spotify Preview Link: " + song.external_urls.spotify,
+            "Album Title: " + song.album.name
+        ].join("\n\n");
+        console.log("\n" + showSong + "\n");
+    })
+        .catch(function (err) {
+            console.log(err);
+        });
 }
 else if (fourCommands === "movie-this") {
+    // console.log("C");
 
     // Then run a request to the OMDB API with the movie specified
     var queryUrl = "http://www.omdbapi.com/?t=" + searchItem + "&y=&plot=short&apikey=trilogy";
@@ -28,18 +66,18 @@ else if (fourCommands === "movie-this") {
             "IMDB Rating: " + movie.imdbRating,
             "Rotten Tomatoes Rating: " + movie.Ratings[1].Value,
             "Country Movie was Produced in: " + movie.Country,
-            "Language: " + movie.Language,
+            "Language(s): " + movie.Language,
             "Plot: " + movie.Plot,
-            "Actors" + movie.Actors
+            "Actors: " + movie.Actors
         ].join("\n\n");
         // If the request is successful
         if (!error && response.statusCode === 200) {
-            console.log(showMovie);
+            console.log("\n" + showMovie + "\n");
         }
     });
 }
 else if (fourCommands === "do-what-it-says") {
-    console.log("D");
+    // console.log("D");
     fs.readFile("random.txt", "utf8", function (error, data) {
         // If the code experiences any errors it will log the error to the console.
         if (error)
@@ -52,8 +90,6 @@ else if (fourCommands === "do-what-it-says") {
         var dataArr = data.split(",");
 
         // We will then re-display the content as an array for later use.
-        console.log(dataArr);
+        console.log("\n" + dataArr + "\n");
     })
 }
-
-
